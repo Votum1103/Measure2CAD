b600b5bb778a106466009723a0ae4a0a278025b8
Votum <Votum1103@gmail.com>
2025-08-10 22:45:02 +0200

    Added showing UI window

--- Class1.cs
+++ Class1.cs
@@ -1,7 +1,23 @@
-﻿namespace Makro_PP
-{
-    public class Class1
-    {
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.Windows;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interop;
 
+
+namespace Makro_PP
+{
+    public class Methods
+    {
+        [CommandMethod("OpenMyUi")]
+        public void OpenMyUi()
+        {
+            var window = new Window1();
+            var helper = new WindowInteropHelper(window);
+            helper.Owner = Autodesk.AutoCAD.ApplicationServices.Application.MainWindow.Handle;
+            window.ShowDialog();
+        }
     }
 }
--- Window1.xaml.cs
+++ Window1.xaml.cs
@@ -1,22 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Makro_PP
 {
-    /// <summary>
-    /// Logika interakcji dla klasy Window1.xaml
-    /// </summary>
     public partial class Window1 : Window
     {
         public Window1()
@@ -24,24 +10,54 @@ namespace Makro_PP
             InitializeComponent();
         }
 
-        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
-        private void RadioButton_Checked(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
-        {
-
-        }
-
+        // Obsługa zdarzeń z zakładki "Pomiary"
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            // Implementacja wspólna dla wszystkich przycisków
+            var button = (Button)sender;
+            switch (button.Content.ToString())
+            {
+                case "Rozpocznij pomiar":
+                    // Logika dla rozpoczęcia pomiaru
+                    break;
+                case "Pomierz punkt":
+                    // Logika dla pomiaru punktu
+                    break;
+                case "Cofnij punkt":
+                    // Logika dla cofnięcia punktu
+                    break;
+                case "Przestaw tachimetr":
+                    // Logika dla przestawienia tachimetru
+                    break;
+            }
+        }
 
+        // Obsługa zdarzenia TextChanged dla TextBox
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Logika gdy zmienia się tekst w TextBox
+        }
+
+        // Obsługa zdarzeń z zakładki "Komendy CAD"
+        private void InsertLine_Click(object sender, RoutedEventArgs e)
+        {
+            // Logika dla wstawienia linii
+        }
+
+        private void InsertPoint_Click(object sender, RoutedEventArgs e)
+        {
+            // Logika dla wstawienia punktu
+        }
+
+        private void InsertArc_Click(object sender, RoutedEventArgs e)
+        {
+            // Logika dla wstawienia łuku
+        }
+
+        // Obsługa zdarzeń RadioButton
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+            // Logika dla wyboru opcji przesyłanego tekstu
         }
     }
 }
